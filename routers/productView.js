7042c9a976238f90c481b8155e4bfae21dfc2449
DESKTOP-0RFM3KA\Blue <wwwbluesky@livemail.tw>
2024-03-30 17:38:34 +0800

    add productDetailView

--- routers/productView.js
+++ routers/productView.js
@@ -25,4 +25,19 @@ router.get("/productList", async (req, res) => {
 
   res.redirect("/test");
 });
+
+router.get("/productDetail", async (req, res) => {
+  const p_id = req.query.p_id;
+  const sub_category_list = await product_model.getAllSubCategory();
+  const dbRes = await product_model.getProduct(p_id);
+  if (dbRes.status == "success" && sub_category_list.status == "success") {
+    return res.render("productDetail", {
+      title: "產品詳情",
+      ...dbRes,
+      sub_category_list: sub_category_list.data,
+    });
+  }
+
+  res.redirect("/test");
+});
 export default router;
