635fd0ee411dfdf5c59d8eef8edf5802b0c55c1d
DESKTOP-0RFM3KA\Blue <wwwbluesky@livemail.tw>
2024-04-07 22:50:06 +0800

    update productDetail view

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = process.eventNames.Port || 3000;
 //view引擎
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 app.use("/product", productRouter);
 app.use("/category", categoryRouter);
